2e27294045d187277ede7e1e05c4b7a0c10e05ff
Nori <loveandfight2186@yahoo.co.jp>
2024-04-06 11:41:02 -0700

    implement product form page, index page

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module ECommerce
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.active_storage.multiple_file_field_include_hidden = false
 
     # Configuration for the application, engines, and railties goes here.
     #
