53a86501ca19a89a0c1dc42b51db7611384cb626
Nori <loveandfight2186@yahoo.co.jp>
2024-04-06 16:05:06 -0700

    implement buy now function

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   root "products#index"
 
   get "/carts/:id", to: "carts#show", as: "carts"
+  post "line_items_buy_now/:product_id", to: "line_items#buy_now", as: "line_item_buy_now"
+  post "line_items_add_to_cart/:product_id", to: "line_items#add_to_cart", as: "line_item_add_to_cart"
 end
