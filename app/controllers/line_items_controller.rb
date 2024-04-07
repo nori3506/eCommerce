a522877817638bc7a993345e1c4554da475cc5e1
Nori <loveandfight2186@yahoo.co.jp>
2024-04-06 21:10:43 -0700

    add turbo Hotwire for subtotal

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,14 +1,77 @@
 class LineItemsController < ApplicationController
+
   def buy_now
+    check_and_add_to_cart
+    redirect_to carts_path(@current_cart)
+  end
+
+  def add_to_cart
+    check_and_add_to_cart
+    if @line_item.quantity == 1
+      @line_item.quantity += (params[:quantity].to_i - 1)
+    else
+      @line_item.quantity += params[:quantity].to_i
+    end
+    @line_item.save
+    redirect_to product_path(@selected_product)
+  end
+
+  def destroy_from_cart
+    @selected_product = Product.find(params[:product_id])
+    @line_item = @current_cart.line_items.find_by(product_id: @selected_product)
+    @line_item.destroy
+    redirect_to carts_path(@current_cart)
+  end
+
+  def add_quantity
+    @selected_product = Product.find(params[:product_id])
+    @line_item = @current_cart.line_items.find_by(product_id: @selected_product)
+    @line_item.quantity += 1
+    @line_item.save
+    # redirect_to carts_path(@current_cart)
+    respond_to do |format|
+      format.turbo_stream {
+        render turbo_stream:
+        turbo_stream.update(
+          "quntity_#{@line_item.id}",
+          partial: "carts/quantity",
+          locals: {line_item: @line_item}
+        )
+       }
+    end
+  end
+
+  def subtract_quantity
+     @selected_product = Product.find(params[:product_id])
+    @line_item = @current_cart.line_items.find_by(product_id: @selected_product)
+    @line_item.quantity -= 1
+    @line_item.save
+
+    if @line_item.quantity == 0
+      @line_item.destroy
+    end
+
+    respond_to do |format|
+      format.turbo_stream {
+        render turbo_stream:
+        turbo_stream.update(
+          "quntity_#{@line_item.id}",
+          partial: "carts/quantity",
+          locals: {line_item: @line_item}
+        )
+        }
+    end
+  end
+
+  def check_and_add_to_cart
     @selected_product = Product.find(params[:product_id])
     if @current_cart.products.include?(@selected_product)
-      @line_item = @current_cart¥.line_items.find_by(product_id: @selected_product.id)
+      @line_item = @current_cart.line_items.find_by(product_id: @selected_product)
     else
       @line_item = LineItem.new
       @line_item.cart = @current_cart
       @line_item.product = @selected_product
       @line_item.save
     end
-    redirect_to carts_path(@current_cart)
   end
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,13 @@
 class Cart < ApplicationRecord
   belongs_to :user
-  has_many :line_items
+  has_many :line_items # current_cart.line_items
   has_many :products, through: :line_items
+
+  def sub_total
+    sum = 0
+    self.line_items.each do |line_item|
+      sum += line_item.total_of_single_product
+    end
+    return sum
+  end
 end
